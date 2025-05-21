05f730b86a97353dd670c36bb8bee7993848a19d
Brigitte Blau <brigitteyaelblau@gmail.com>
2025-05-20 22:18:30 -0300

    fix: update unit price in PaymentButton and remove unused doGet function

--- src/components/PaymentButton.jsx
+++ src/components/PaymentButton.jsx
@@ -19,7 +19,7 @@ const PaymentButton = ({ email }) => {
             {
               title: 'Tevila - Servicio',
               quantity: 1,
-              unit_price: 15000
+              unit_price: 1
             }
           ],
           payer: {
